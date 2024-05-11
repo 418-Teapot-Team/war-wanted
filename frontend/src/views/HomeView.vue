afcbbc17534518b9c4d594f5b00aa3e4dcc5d9ee
Kolia913 <mynew1mail470@gmail.com>
2024-05-11 18:07:48 +0300

    fix: error page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
   <AppPlainInput />
+  <AppButton text="Hello World!" />
 </template>
 
 <script setup>
 import AppPlainInput from '@/components/atoms/inputs/AppPlainInput.vue';
+import AppButton from '@/components/atoms/buttons/AppButton.vue';
 </script>
