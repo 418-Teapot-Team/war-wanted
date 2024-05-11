4127474a5f39ec3724eb152f5c4c03df1f3bd507
Alex <oleksii.uzhva.pz.2021@lpnu.ua>
2024-05-11 20:29:30 +0300

    feat: form folder

--- frontend-public/src/views/HomeView.vue
+++ frontend-public/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <AppPlainInput />
   <AppButton text="Hello World!" />
+  <FormAddInfo />
 </template>
 
 <script setup>
 import AppPlainInput from '@/components/atoms/inputs/AppPlainInput.vue';
 import AppButton from '@/components/atoms/buttons/AppButton.vue';
+import FormAddInfo from '@/components/form/FormAddInfo.vue';
 </script>
