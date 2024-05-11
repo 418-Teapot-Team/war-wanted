e1955b680b54ec942037ff116486cbea68978e60
Alex <oleksii.uzhva.pz.2021@lpnu.ua>
2024-05-11 22:13:23 +0300

    feat: basic form elements

--- frontend-public/src/views/HomeView.vue
+++ frontend-public/src/views/HomeView.vue
@@ -1,11 +1,7 @@
 <template>
-  <AppPlainInput />
-  <AppButton text="Hello World!" />
   <FormAddInfo />
 </template>
 
 <script setup>
-import AppPlainInput from '@/components/atoms/inputs/AppPlainInput.vue';
-import AppButton from '@/components/atoms/buttons/AppButton.vue';
 import FormAddInfo from '@/components/form/FormAddInfo.vue';
 </script>
