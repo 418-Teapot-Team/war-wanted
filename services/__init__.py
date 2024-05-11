5e8a18ef35f0317beda106fd670b7b9b94a1ece8
Tymofii Nasobko <44300490+TimofiyJ@users.noreply.github.com>
2024-05-12 02:03:53 +0300

    refactor: sync project schema

--- services/__init__.py
+++
@@ -1,7 +0,0 @@
-from .getting import getting_bp
-from .posting import posting_bp
-
-from .controllers import api_bp
-
-api_bp.register_blueprint(getting_bp)
-api_bp.register_blueprint(posting_bp)
--- services/getting.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request, jsonify
-
-getting_bp = Blueprint("get", __name__, url_prefix="/get")
-
-@getting_bp.route("/signup", methods=["POST"])
-def signup():
-    data = request.get_json()
-    return jsonify(message="Invalid role"), 400
