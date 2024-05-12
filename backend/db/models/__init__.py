7f31eb2322da2968ba1e0bfb953624c205f1a6fb
Roman Skok <rma.skok@gmail.com>
2024-05-12 10:07:37 +0300

    feat: matches

--- backend/db/models/__init__.py
+++ backend/db/models/__init__.py
@@ -1,5 +1,7 @@
 from .user import User
 from .found_person import FoundPerson
+from .in_search_person import InSearchPerson
+from .possible_match import PossibleMatch
 
 
-__all__ = ["User", "FoundPerson"]
+__all__ = ["User", "FoundPerson", "InSearchPerson", "PossibleMatch"]
