e46b7a6aaa8154bc406824fae3d2c8dca19952ad
Kolia913 <mynew1mail470@gmail.com>
2024-05-12 10:35:32 +0300

    feat: cards list

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,22 +1,21 @@
 <template>
   <div class="grid grid-cols-1 xl:grid-cols-2 gap-8 w-full">
-    <AppCard />
-    <AppCard />
-    <AppCard />
-    <AppCard />
-    <AppCard />
-    <AppCard />
-    <AppCard />
-    <AppCard />
-    <AppCard />
-    <AppCard />
-    <AppCard />
-    <AppCard />
-    <AppCard />
-    <AppCard />
+    <AppCard v-for="item in data" :key="item.id" :data="item" />
   </div>
 </template>
 
 <script setup>
 import AppCard from '@/components/AppCard.vue';
+
+import { useIndexStore } from '@/stores';
+import { onBeforeMount, ref } from 'vue';
+const store = useIndexStore();
+
+const data = ref([]);
+
+onBeforeMount(() => {
+  store.fetchAll().then((res) => {
+    data.value = res.result;
+  });
+});
 </script>
